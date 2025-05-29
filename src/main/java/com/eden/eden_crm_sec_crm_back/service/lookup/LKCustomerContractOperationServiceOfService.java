bf746a11caba0f484e04a8ecbdaf7d3142e83768
Ahmed Hesham <ahmed.hesham@eden-tech.io>
2025-05-29 15:17:51 +0300

    delete useless code

--- src/main/java/com/eden/eden_crm_sec_crm_back/service/lookup/LKCustomerContractOperationServiceOfService.java
+++
@@ -1,6 +0,0 @@
-package com.eden.eden_crm_sec_crm_back.service.lookup;
-import com.eden.eden_crm_sec_crm_back.base.service.BaseService;
-import com.eden.eden_crm_sec_crm_back.models.lookup.LKCustomerContractOperationService;
-
-public interface LKCustomerContractOperationServiceOfService extends BaseService<LKCustomerContractOperationService, Long> {
-}
