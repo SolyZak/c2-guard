8ee36a93acb542399529dab24f7f489613c4a927
Ahmed Hesham <ahmed.hesham@eden-tech.io>
2025-06-10 16:05:14 +0300

    enhance in enum

--- src/main/java/com/eden/eden_crm_sec_crm_back/enums/UnitEnum.java
+++ src/main/java/com/eden/eden_crm_sec_crm_back/enums/UnitEnum.java
@@ -1,21 +1,6 @@
 package com.eden.eden_crm_sec_crm_back.enums;
 
-import lombok.Getter;
-
-@Getter
 public enum UnitEnum {
-
-    PERSON("PERSON", "فرد", "Person"),
-    PRODUCT("PRODUCT", "منتج", "Product");
-
-    private final String code;
-    private final String nameAr;
-    private final String nameEn;
-
-    UnitEnum(String code, String nameAr, String nameEn) {
-        this.code = code;
-        this.nameAr = nameAr;
-        this.nameEn = nameEn;
-    }
-
+    PERSON,
+    PRODUCT
 }
