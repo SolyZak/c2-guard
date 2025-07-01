ad6e2f12583040f88dd0de8425fb7f57f415c8e4
Ahmed Hesham <ahmed.hesham@eden-tech.io>
2025-07-01 17:29:26 +0300

    solve typo issue

--- src/main/java/com/eden/eden_crm_sec_crm_back/repository/CustomerSiteRepository.java
+++ src/main/java/com/eden/eden_crm_sec_crm_back/repository/CustomerSiteRepository.java
@@ -60,7 +60,7 @@ public interface CustomerSiteRepository extends JpaRepository<CustomerSite, Long
                     SELECT sd FROM SiteDistribution sd
                     WHERE sd.site = cs
                     AND sd.customerContract.securityCompanyId = :securityCompanyId
-                    AND sd.customerContract.startAgreementDate >= :today
+                    AND sd.customerContract.startAgreementDate <= :today
                     AND sd.customerContract.endAgreementDate >= :today
                 )
             """)
