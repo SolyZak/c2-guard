fb1d5f1156de10fb3581fc66e796e916173e2565
adeleden2025 <adel.salah@eden-tech.io>
2025-10-15 17:16:43 +0300

    List Locations US

--- src/main/java/com/eden/eden_crm_sec_crm_back/controller/LocationController.java
+++ src/main/java/com/eden/eden_crm_sec_crm_back/controller/LocationController.java
@@ -1,17 +1,14 @@
 package com.eden.eden_crm_sec_crm_back.controller;
 
 import com.eden.eden_crm_sec_crm_back.dto.request.AddLocationRequest;
-import com.eden.eden_crm_sec_crm_back.dto.request.PremiseRequestDto;
-import com.eden.eden_crm_sec_crm_back.dto.response.PremiseResponseDto;
+import com.eden.eden_crm_sec_crm_back.dto.response.PremiseLocationDto;
 import com.eden.eden_crm_sec_crm_back.payload.ApiResponse;
+import com.eden.eden_crm_sec_crm_back.payload.PaginateResponse;
 import com.eden.eden_crm_sec_crm_back.service.LocationService;
 import com.google.zxing.WriterException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -22,8 +19,15 @@ public class LocationController {
 
     private final LocationService locationService;
     @PostMapping
-    ApiResponse<PremiseResponseDto> addLocation(@RequestBody @Valid AddLocationRequest request) throws IOException, WriterException {
+    ApiResponse addLocation(@RequestBody @Valid AddLocationRequest request) throws IOException, WriterException {
         locationService.addNewLocation(request);
         return ApiResponse.created();
+    }
+
+    @GetMapping
+    ApiResponse<PaginateResponse<PremiseLocationDto>> listLocations(@RequestParam(defaultValue = "0", name = "page") Integer page,
+                                                                    @RequestParam(defaultValue = "10", name = "size") Integer size,
+                                                                    @RequestParam(required = false, name = "search") String search) {
+        return ApiResponse.ok(locationService.getLocationsPaginated(search, page, size));
     }
 }
--- src/main/java/com/eden/eden_crm_sec_crm_back/service/impl/LocationServiceImpl.java
+++ src/main/java/com/eden/eden_crm_sec_crm_back/service/impl/LocationServiceImpl.java
@@ -2,24 +2,39 @@ package com.eden.eden_crm_sec_crm_back.service.impl;
 
 import com.eden.eden_crm_sec_crm_back.dto.request.AddLocationRequest;
 import com.eden.eden_crm_sec_crm_back.dto.request.LocationDto;
+import com.eden.eden_crm_sec_crm_back.dto.response.PremiseLocationDto;
 import com.eden.eden_crm_sec_crm_back.enums.LocationAccessTypeEnum;
 import com.eden.eden_crm_sec_crm_back.exception.BusinessException;
 import com.eden.eden_crm_sec_crm_back.exception.PremiseNotProvided;
 import com.eden.eden_crm_sec_crm_back.models.Location;
 import com.eden.eden_crm_sec_crm_back.models.Premise;
+import com.eden.eden_crm_sec_crm_back.models.projections.LocationProjection;
+import com.eden.eden_crm_sec_crm_back.payload.PaginateResponse;
 import com.eden.eden_crm_sec_crm_back.repository.LocationRepository;
 import com.eden.eden_crm_sec_crm_back.repository.PremiseRepository;
 import com.eden.eden_crm_sec_crm_back.service.LocationService;
 import com.eden.eden_crm_sec_crm_back.utils.MessageUtil;
 import com.eden.eden_crm_sec_crm_back.utils.QrCodeUtil;
 import com.google.zxing.WriterException;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.postgresql.PGConnection;
+import org.postgresql.largeobject.LargeObject;
+import org.postgresql.largeobject.LargeObjectManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,6 +43,8 @@ import java.util.Optional;
 public class LocationServiceImpl implements LocationService {
     private final LocationRepository locationRepository;
     private final PremiseRepository premiseRepository;
+
+    private final EntityManager em;
 
     @Override
     @Transactional
@@ -55,5 +72,47 @@ public class LocationServiceImpl implements LocationService {
             }
             locationRepository.saveAll(locations);
         }
+    }
+
+    @Override
+    public PaginateResponse<PremiseLocationDto> getLocationsPaginated(String search, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "id"));
+        Page<LocationProjection> resultPage = locationRepository.searchByPremiseNameAndLocationNameAndAccessType(search, pageable);
+        List<PremiseLocationDto> premiseLocationDtos = new ArrayList<>();
+        if (resultPage.getContent() != null) {
+            for (LocationProjection location : resultPage.getContent()) {
+                PremiseLocationDto dto = new PremiseLocationDto(location.getId(), location.getName(), location.getAccessType(),
+                        location.getPremise() != null ? location.getPremise().getName() : "",
+                        location.getAccessType().equals(LocationAccessTypeEnum.SPECIFIC_POINT.getType())  ? "" : Base64.getEncoder().encodeToString(getQrImage(location.getId())));
+                premiseLocationDtos.add(dto);
+            }
+        }
+        return new PaginateResponse<>(
+                premiseLocationDtos,
+                page,
+                size,
+                resultPage.getTotalElements(),
+                (long) resultPage.getTotalPages()
+        );
+    }
+    public byte[] getQrImage(Long id) {
+        Session session = em.unwrap(Session.class);
+        return session.doReturningWork(connection -> {
+            connection.setAutoCommit(false);
+            try (PreparedStatement ps = connection.prepareStatement("SELECT qr_image FROM location WHERE id = ?")) {
+                ps.setLong(1, id);
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    long oid = rs.getLong(1);
+                    PGConnection pgConnection = connection.unwrap(PGConnection.class);
+                    LargeObjectManager lobj = pgConnection.getLargeObjectAPI();
+                    LargeObject obj = lobj.open(oid, LargeObjectManager.READ);
+                    byte[] data = obj.read((int) obj.size());
+                    obj.close();
+                    return data;
+                }
+                return null;
+            }
+        });
     }
 }
