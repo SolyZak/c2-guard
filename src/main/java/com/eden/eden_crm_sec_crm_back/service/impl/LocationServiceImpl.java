3383fe8b5fdb0271096579c577aa2d3f1e6ea2d1
Ahmed Ezzat <ahmed.ezzat@eden-tech.io>
2025-12-22 15:35:49 +0200

    Fix issue in locations

--- src/main/java/com/eden/eden_crm_sec_crm_back/service/impl/LocationServiceImpl.java
+++ src/main/java/com/eden/eden_crm_sec_crm_back/service/impl/LocationServiceImpl.java
@@ -166,6 +166,8 @@ public class LocationServiceImpl implements LocationService {
                     return data;
                 }
                 return null;
+            } catch (Exception e) {
+                return new byte[0];
             }
         });
 
