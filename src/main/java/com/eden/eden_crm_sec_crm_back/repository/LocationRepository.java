78f658fdbc8331f22fee1294a7cbad5f62612bc8
Nour El-Deen <noureldin.mounir@eden-tech.io>
2025-12-30 15:29:11 +0200

    Add long lat and tolerance to API crm/location

--- src/main/java/com/eden/eden_crm_sec_crm_back/repository/LocationRepository.java
+++ src/main/java/com/eden/eden_crm_sec_crm_back/repository/LocationRepository.java
@@ -15,29 +15,58 @@ import java.util.Set;
 
 public interface LocationRepository extends JpaRepository<Location,Long> {
     @Query("""
-                SELECT l.id as id,l.premise as premise,l.name as name,l.accessType as accessType,l.longitude as longitude,l.latitude as latitude FROM Location l JOIN Premise p on p.id = l.premise.id where l.customer.id = :customerId AND (lower(p.name) like lower(concat('%', :search, '%')) OR lower(l.name) like lower(concat('%', :search, '%')) OR lower(l.accessType) like lower(concat('%', :search, '%')) OR :search is null)
-            """)
-    Page<LocationProjection> searchByPremiseNameAndLocationNameAndAccessType(@Param("search") String search, @Param("customerId") Long customerId,
-                                                                             Pageable pageable);
+    SELECT l.id as id,
+           l.premise as premise,
+           l.name as name,
+           l.accessType as accessType,
+           l.longitude as longitude,
+           l.latitude as latitude,
+           l.tolerance as tolerance
+    FROM Location l 
+    JOIN Premise p on p.id = l.premise.id 
+    WHERE l.customer.id = :customerId 
+    AND (lower(p.name) like lower(concat('%', :search, '%')) 
+         OR lower(l.name) like lower(concat('%', :search, '%')) 
+         OR lower(l.accessType) like lower(concat('%', :search, '%')) 
+         OR :search is null)
+""")
+    Page<LocationProjection> searchByPremiseNameAndLocationNameAndAccessType(
+            @Param("search") String search,
+            @Param("customerId") Long customerId,
+            Pageable pageable);
+
     @Query(value = """
             select l.name from location l INNER JOIN patrol_detail lpd on l.id = lpd.location_id where lpd.id = :detailsId 
             """, nativeQuery = true)
     List<String> getLocationNamesByDetailId(@Param("detailsId") Long detailsId);
 
     @Query("""
-            SELECT l.id as id,l.premise as premise,l.name as name,l.accessType as accessType,l.longitude as longitude,l.latitude as latitude FROM Location l JOIN Premise p on p.id = l.premise.id where l.customer.id = :customerId
-            """)
+    SELECT l.id as id, l.premise as premise, l.name as name, l.accessType as accessType,
+           l.longitude as longitude, l.latitude as latitude, l.tolerance as tolerance 
+    FROM Location l JOIN Premise p on p.id = l.premise.id 
+    WHERE l.customer.id = :customerId
+""")
     List<LocationProjection> listAllLoggedInCustomerLocations(@Param("customerId") Long customerId);
 
     @Query("""
-            SELECT l.id as id,l.name as name,l.accessType as accessType,l.longitude as longitude,l.latitude as latitude FROM Location l where l.customer.id = :customerId and id in :locationIds
-            """)
-    List<LocationProjection> listAllLoggedInCustomerLocationsByIds(@Param("customerId") Long customerId, @Param("locationIds")Set<Long> locationIds);
+    SELECT l.id as id, l.name as name, l.accessType as accessType,
+           l.longitude as longitude, l.latitude as latitude, l.tolerance as tolerance 
+    FROM Location l 
+    WHERE l.customer.id = :customerId and id in :locationIds
+""")
+    List<LocationProjection> listAllLoggedInCustomerLocationsByIds(
+            @Param("customerId") Long customerId,
+            @Param("locationIds") Set<Long> locationIds);
 
     @Query("""
-            SELECT l.id as id,l.premise as premise,l.name as name,l.accessType as accessType,l.longitude as longitude,l.latitude as latitude FROM Location l JOIN l.patrolDetails pd where l.customer.id = :customerId AND pd.patrol.id = :patrolId
-            """)
-    List<LocationProjection> listAllLoggedInCustomerLocationsByPatrolId(@Param("customerId") Long customerId, @Param("patrolId") Long patrolId);
+    SELECT l.id as id, l.premise as premise, l.name as name, l.accessType as accessType,
+           l.longitude as longitude, l.latitude as latitude, l.tolerance as tolerance 
+    FROM Location l JOIN l.patrolDetails pd 
+    WHERE l.customer.id = :customerId AND pd.patrol.id = :patrolId
+""")
+    List<LocationProjection> listAllLoggedInCustomerLocationsByPatrolId(
+            @Param("customerId") Long customerId,
+            @Param("patrolId") Long patrolId);
 
     @Query("SELECT l.accessType FROM Location l WHERE l.id = :id")
     Optional<String> findAccessTypeById(@Param("id") Long id);
