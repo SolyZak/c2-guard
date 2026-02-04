a063a93fdb4421c2e108921ceaa239a13730a727
Ahmed Ezzat <ahmed.ezzat@eden-tech.io>
2026-02-04 15:51:42 +0200

    Add getDistributableTasks endpoint

--- src/main/java/com/eden/eden_crm_sec_crm_back/taskdistribution/mappers/TaskDistributionMapper.java
+++ src/main/java/com/eden/eden_crm_sec_crm_back/taskdistribution/mappers/TaskDistributionMapper.java
@@ -1,7 +1,9 @@
 package com.eden.eden_crm_sec_crm_back.taskdistribution.mappers;
 
+import com.eden.eden_crm_sec_crm_back.taskdistribution.dtos.response.DistributableTaskResponse;
 import com.eden.eden_crm_sec_crm_back.taskdistribution.dtos.response.TodayTaskEntryResponse;
 import com.eden.eden_crm_sec_crm_back.taskdistribution.dtos.response.TodayTaskExecutionSlotEntryResponse;
+import com.eden.eden_crm_sec_crm_back.taskdistribution.repositories.projections.DistributableTaskProjection;
 import com.eden.eden_crm_sec_crm_back.taskdistribution.repositories.projections.TodayTaskSlotProjection;
 import org.mapstruct.Context;
 import org.mapstruct.Mapper;
@@ -20,4 +22,8 @@ public interface TaskDistributionMapper {
     @Mapping(target = "patrolFrequencyRate", source = "frequencyRate")
     @Mapping(target = "executionSlots", expression = "java(executionSlots)")
     TodayTaskEntryResponse toTodayTaskEntryResponse(TodayTaskSlotProjection projection, @Context List<TodayTaskExecutionSlotEntryResponse> executionSlots);
+
+    DistributableTaskResponse toDistributableTaskResponse(DistributableTaskProjection distributableTaskProjection);
+
+    List<DistributableTaskResponse> toDistributableTaskResponseList(List<DistributableTaskProjection> distributableTaskProjections);
 }
