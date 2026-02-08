d0c71411584ef1b2319ea80b2717a320f4a861aa
Ahmed Ezzat <ahmed.ezzat@eden-tech.io>
2026-02-08 09:27:08 +0200

    Adapt validate qr to the latest changes

--- src/main/java/com/eden/eden_crm_sec_crm_back/dto/request/ValidateQrRequest.java
+++ src/main/java/com/eden/eden_crm_sec_crm_back/dto/request/ValidateQrRequest.java
@@ -2,24 +2,14 @@ package com.eden.eden_crm_sec_crm_back.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
+import lombok.Builder;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class ValidateQrRequest {
-
-    @NotBlank(message = "Payload cannot be blank")
-    private String payload;   // raw text from the QR code
-
-    @NotNull(message = "Patrol Distribution ID is required")
-    private Long patrolDistributionId;
-
-    @NotNull(message = "Task ID is required")
-    private Long taskId;
-
-}
+@Builder
+public record ValidateQrRequest(
+    @NotBlank
+    String payload,
+    @NotNull
+    Long patrolDistributionId,
+    @NotNull
+    Long taskId
+) {}
