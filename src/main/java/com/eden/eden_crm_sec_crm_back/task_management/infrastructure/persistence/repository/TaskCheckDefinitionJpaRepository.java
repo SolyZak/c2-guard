a78db35a472117cd028423acfcc70df8f282a63d
Nour El-Deen <noureldin.mounir@eden-tech.io>
2026-03-02 14:57:50 +0200

    Image upload fix V2

--- src/main/java/com/eden/eden_crm_sec_crm_back/task_management/infrastructure/persistence/repository/TaskCheckDefinitionJpaRepository.java
+++ src/main/java/com/eden/eden_crm_sec_crm_back/task_management/infrastructure/persistence/repository/TaskCheckDefinitionJpaRepository.java
@@ -14,29 +14,30 @@ public interface TaskCheckDefinitionJpaRepository extends JpaRepository<TaskChec
     List<TaskCheckDefinitionJpaEntity> findAllByTaskDefinitionIdAndDeletedAtIsNull(@Param("taskDefinitionId") Long taskDefinitionId);
 
     @Query(nativeQuery = true, value = """
-            SELECT
-                l.id            AS lodcationId,
-                l.name          AS locationName,
-                td.id           AS taskDefinitionId,
-                td.name         AS taskDefinitionName,
-                tcd.id          AS checkId,
-                tcd.name        AS checkName,
-                tcd.image_url   AS imageUrl
-            FROM location l
-                     INNER JOIN patrol_detail pd
-                                ON pd.location_id = l.id
-                     INNER JOIN task_definition td
-                                ON td.id = pd.task_definition_id
-                                    AND td.customer_id = :customerId
-                                    AND td.deleted_at IS NULL
-                     INNER JOIN task_check_definition tcd
-                                ON tcd.task_definition_id = td.id
-                                    AND tcd.customer_id = :customerId
-                                    AND tcd.deleted_at IS NULL
-            WHERE l.premise_id  = :premiseId
-              AND l.customer_id = :customerId
-            ORDER BY l.id, td.id, tcd.id
-        """)
+    SELECT
+        l.id            AS locationId,
+        l.name          AS locationName,
+        td.id           AS taskDefinitionId,
+        td.name         AS taskDefinitionName,
+        tcd.id          AS checkId,
+        tcd.name        AS checkName,
+        tcd.image_url   AS imageUrl
+    FROM location l
+             INNER JOIN patrol_detail pd
+                        ON pd.location_id = l.id
+                        AND pd.task_definition_id IS NOT NULL
+             INNER JOIN task_definition td
+                        ON td.id = pd.task_definition_id
+                            AND td.customer_id = :customerId
+                            AND td.deleted_at IS NULL
+             INNER JOIN task_check_definition tcd
+                        ON tcd.task_definition_id = td.id
+                            AND tcd.customer_id = :customerId
+                            AND tcd.deleted_at IS NULL
+    WHERE l.premise_id  = :premiseId
+      AND l.customer_id = :customerId
+    ORDER BY l.id, td.id, tcd.id
+    """)
     <T> List<T> findAllChecksByPremiseAndCustomer(
             @Param("premiseId") Long premiseId,
             @Param("customerId") Long customerId,
