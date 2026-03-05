cacdd86fe80d27630a11c61645ed0d797082be89
ahmedHossam87 <ahmed.hossam@eden-tech.io>
2026-03-05 12:03:48 +0200

    fix (#300)

--- src/main/java/com/eden/eden_crm_sec_crm_back/task_management/domain/valueobject/checkvalue/DecimalCheckValue.java
+++ src/main/java/com/eden/eden_crm_sec_crm_back/task_management/domain/valueobject/checkvalue/DecimalCheckValue.java
@@ -1,10 +1,13 @@
 package com.eden.eden_crm_sec_crm_back.task_management.domain.valueobject.checkvalue;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Set;
 
 /**
  * Check value for decimal numeric inputs with an operator constraint.
@@ -17,12 +20,19 @@ import lombok.Setter;
 @AllArgsConstructor
 public class DecimalCheckValue extends TaskCheckValue {
 
+    private static final Set<String> VALID_OPERATORS = Set.of("gte", "lte", "gt", "lt", "eq", "ne");
+
     private String unit;
+    @Setter(AccessLevel.NONE)
     private String operator;
     private Double value;
 
+    public void setOperator(String operator) {
+        this.operator = normalizeOperator(operator);
+    }
+
     @Override
     public boolean isValid() {
-        return operator != null && !operator.isBlank() && value != null;
+        return operator != null && VALID_OPERATORS.contains(operator) && value != null;
     }
 }
