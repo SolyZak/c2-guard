a2fa356420299242a160dad5dd099170ffe2ef1d
Nour El-Deen <noureldin.mounir@eden-tech.io>
2026-03-12 13:27:43 +0200

    image comparison APIs update V2

--- src/main/java/com/eden/eden_crm_sec_crm_back/clients/ImageComparisonFeignClient.java
+++ src/main/java/com/eden/eden_crm_sec_crm_back/clients/ImageComparisonFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
-        name = "image-comparison-service"
+        name = "pcd-ai-service"
 )
 public interface ImageComparisonFeignClient {
 
