6dca32c56d1964e5c8ddb73d624df8fece42d94a
Nour El-Deen <noureldin.mounir@eden-tech.io>
2026-05-20 13:15:42 +0300

    added checkout after time tolerance.

--- src/main/resources/db/migration/V81__add_check_out_after_minutes_to_contract_operation_rules.sql
+++ src/main/resources/db/migration/V81__add_check_out_after_minutes_to_contract_operation_rules.sql
@@ -1,2 +1,6 @@
 ALTER TABLE public.contract_operation_rules
-    ADD COLUMN IF NOT EXISTS check_out_after_minutes INTEGER;
+    ADD COLUMN IF NOT EXISTS check_out_after_minutes INTEGER DEFAULT 10;
+
+UPDATE public.contract_operation_rules
+SET check_out_after_minutes = 10
+WHERE check_out_after_minutes IS NULL;
